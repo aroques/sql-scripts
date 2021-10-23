6ce4cfec792874030fb68bb09e7a080393249911
Alec <alec.roques@gmail.com>
2021-10-23 09:50:50 -0500

    Update sp_FindEFCoreQuery.sql

--- sp_FindEFCoreQuery.sql
+++ sp_FindEFCoreQuery.sql
@@ -9,13 +9,16 @@ AS
 */
 BEGIN
 
+DECLARE @DatabaseName AS NVARCHAR(128) = N'StackOverflow',
+	    @ResultCount AS INT = 0;
+
 SELECT 
 	SqlHandle = qs.[sql_handle], 
 	[Text] = st.[text], 
 	CreationDateTime = qs.creation_time, 
 	ExecutionCnt = qs.execution_count,
 	[Avg Duration (ms)]= CAST(ROUND((qs.total_elapsed_time / 1000.0) / qs.execution_count, 2) AS DECIMAL(18,2)),
-	MoreInfo = 'EXEC dbo.sp_BlitzCache @DatabaseName=''StackOverflow'', @OnlySqlHandles=''' + CONVERT(VARCHAR(MAX), qs.[sql_handle], 1) + ''''
+	MoreInfo = 'EXEC dbo.sp_BlitzCache @DatabaseName=''' + @DatabaseName + ''', @OnlySqlHandles=''' + CONVERT(VARCHAR(MAX), qs.[sql_handle], 1) + ''''
 INTO #sql_handles
 FROM sys.dm_exec_cached_plans cp
 CROSS APPLY sys.dm_exec_sql_text(cp.plan_handle) st
@@ -24,18 +27,24 @@ JOIN sys.dm_exec_query_stats qs ON qs.plan_handle = cp.plan_handle
 WHERE 1=1 
 	AND st.[text] LIKE @QueryText
 	AND cp.objtype= 'Prepared'
-	AND DB_NAME(st.[dbid]) = 'StackOverflow'
+	AND DB_NAME(st.[dbid]) = @DatabaseName
 	AND st.[text] NOT LIKE '%dm_exec_cached_plans%'
 	AND st.[text] NOT LIKE '%##BlitzCacheProcs%'
 
-IF 1 = (SELECT COUNT(*) FROM #sql_handles)
+SELECT @ResultCount = COUNT(*) FROM #sql_handles
+
+IF @ResultCount = 0
+BEGIN 
+	SELECT Result = 'No plans found.'
+END
+ELSE IF @ResultCount = 1
 BEGIN
 	DECLARE @SqlHandle AS VARCHAR(MAX)
 	SELECT @SqlHandle = CONVERT(VARCHAR(MAX), sh.SqlHandle, 1) FROM #sql_handles AS sh
-	SELECT [Executing...] = 'EXEC dbo.sp_BlitzCache @DatabaseName=''StackOverflow'', @OnlySqlHandles=''' + @SqlHandle + ''''
-	EXEC dbo.sp_BlitzCache @DatabaseName='StackOverflow', @OnlySqlHandles=@SqlHandle
+	SELECT [Executing...] = 'EXEC dbo.sp_BlitzCache @DatabaseName=''' + @DatabaseName + ''', @OnlySqlHandles=''' + @SqlHandle + ''''
+	EXEC dbo.sp_BlitzCache @DatabaseName=@DatabaseName, @OnlySqlHandles=@SqlHandle
 END
-ELSE
+ELSE 
 BEGIN
 	SELECT Result = 'More than 1 plan found. Try adding more text to your search so that only 1 plan is found.'
 	SELECT sh.[Text],
